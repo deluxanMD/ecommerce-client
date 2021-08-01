e299fbb867d26f6370242fa3a05f5fb368eff74d
Deluxan Mariathasan <deluxan_m@outlook.com>
2021-08-01 13:28:52 +0530

    register user actions, reducers

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,6 +1,33 @@
+import axios from "axios";
+
 import { TYPES } from "./types";
 
 export const setCurrentUser = (user) => ({
   type: TYPES.SET_CURRENT_USER,
   payload: user,
 });
+
+export const register = (user) => (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  axios
+    .post("http:localhost:5000/api/users", user, config)
+    .then((res) => {
+      console.log(res);
+      dispatch({
+        type: TYPES.SUCCESSFUL_REGISTER,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      dispatch({
+        type: TYPES.FAILURE_REGISTER,
+        payload: err.response.data.errors,
+      });
+    });
+};
--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -3,16 +3,31 @@ import { isEmpty } from "lodash";
 
 const initialState = {
   isAuthenticated: false,
+  token: localStorage.getItem("token"),
   user: {},
 };
 
-const authReducer = (state = initialState, action) => {
-  switch (action.type) {
+const authReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
     case TYPES.SET_CURRENT_USER:
       return {
         ...state,
-        isAuthenticated: !isEmpty(action.payload),
-        user: action.payload,
+        isAuthenticated: !isEmpty(payload),
+        user: payload,
+      };
+    case TYPES.SUCCESSFUL_REGISTER:
+      localStorage.setItem("token", payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+      };
+    case TYPES.FAILURE_REGISTER:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
       };
     default:
       return state;
